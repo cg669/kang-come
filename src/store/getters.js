14ae766b10d1096aaef792180ea349a450fcd4cc
chenguang.zhang <chenguang.zhang@zhangmen.com>
2018-03-27 23:34:00 +0800

    init

---
+++ src/store/getters.js
@@ -0,0 +1,20 @@
+const getters = {
+    // sidebar: state => state.app.sidebar,
+    // route: state => state.app.route,
+    // isConfirmOrderStatus: state => state.app.isConfirmOrderStatus,
+    // inventoryCard: state => state.app.inventoryCard,
+  
+    // token: state => state.user.token,
+    // avatar: state => state.user.avatar,
+    // name: state => state.user.name,
+    // roles: state => state.user.roles,
+    // mobile: state => state.user.mobile,
+    // shopCar: state => state.user.shopCar,
+    // shopCarNum: state => state.user.shopCarNum,
+    // shopCarPrice: state => state.user.shopCarPrice,
+    // orderInfo: state => state.user.orderInfo,
+    // mutiShopCar: state => state.user.mutiShopCar,
+  
+    // reportsPath:state => state.auth.reportsPath // 有权限的路由
+  }
+  export default getters
